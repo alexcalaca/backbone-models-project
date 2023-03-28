3e2a8584ed68311fdc06ef2d30342ffee1357193
Alexandre Calaça <alexcalaca@gmail.com>
2023-03-28 09:22:20 -0300

    feat: enable url function
    By default, the urlRoot property is not defined. This urlRoot property is used by the url method to create a relative URL where the model's resource would be located on the server.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ var Vehicle = Backbone.Model.extend({
   //Vehicle unique identification
   idAttribute: "registrationNumber",
 
+  //Vehicles can be retrieved from the server
+  urlRoot: "/api/vehicles",
+
   //Create a method called start
   start: function(){
     console.log("Vehicle started.");
