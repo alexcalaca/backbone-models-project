9043865ad9ce67b426289f17c374346938e615f9
Alexandre Calaça <alexcalaca@gmail.com>
2023-03-28 09:17:56 -0300

    feat: add object constructor

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 //Create backbone vehicle model 
 var Vehicle = Backbone.Model.extend({
 
+  //Initialize object
+  initialize: function(){
+    console.log("initialized");
+  },
   //Create a method called start
   start: function(){
     console.log("Vehicle started.");
