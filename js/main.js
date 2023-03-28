73c2148312963a039aeb0c41ced632f4ae0b242d
Alexandre Calaça <alexcalaca@gmail.com>
2023-03-28 09:23:11 -0300

    feat: validate registration number presence

--- js/main.js
+++ js/main.js
@@ -13,8 +13,15 @@ var Vehicle = Backbone.Model.extend({
   //Vehicles can be retrieved from the server
   urlRoot: "/api/vehicles",
 
+  //Validate registration number presence
+  validate: function(attrs){
+    if (!attrs.registrationNumber)
+      return "Vehicle is not valid.";
+  },
+
   //Create a method called start
   start: function(){
     console.log("Vehicle started.");
   }
+
 });
